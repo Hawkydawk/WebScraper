051e73fab69a597b4578c5e25923fb6de543f39a
Hawkydawk <icatching@naver.com>
2022-06-24 08:46:13 +0900

    File 7: In progress

--- 7_bs4_sentences.py
+++ 7_bs4_sentences.py
@@ -1,3 +1,12 @@
-def hello():
-    print("Hello Git")
-hello()
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://sentencedict.com/"
+res = requests.get(url)
+res.raise_for_status()
+
+soup = BeautifulSoup(res.text, "lxml")
+sentenceList2 = soup.find_all("a", attrs={"class":"dotline"})
+# for sentenceText in sentenceList2:
+#     print(sentenceText.a.get_text())
+print(sentenceList2)
